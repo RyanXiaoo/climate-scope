1bfdc52ee75a778c0f6573814267c3197b17cc9f
RyanXiaoo <118212529+RyanXiaoo@users.noreply.github.com>
2025-05-09 01:55:53 -0400

    finished navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,16 +3,29 @@ import Image from "next/image";
 const Navbar = () => {
     return (
         <div className="w-full h-20 bg-white shadow-md">
-            <div className="flex items-center h-full px-4">
-                <div className="relative w-16 h-16">
-                    <Image
-                        src="/images/logo.jpg"
-                        alt="logo"
-                        fill={true}
-                        className="object-contain"
-                    />
+            <div className="flex items-center justify-between h-full px-48">
+                <div className="flex items-center">
+                    <div className="relative w-16 h-16">
+                        <Image
+                            src="/images/logo.jpg"
+                            alt="logo"
+                            fill={true}
+                            className="object-contain"
+                        />
+                    </div>
+                    <h1 className="ml-4 text-base font-bold">ClimateScope</h1>
                 </div>
-                <h1 className="ml-4 text-2xl font-bold">ClimateScope</h1>
+                <div className="flex gap-12 items-center">
+                    <a href="/explore" className="hover:text-gray-600">
+                        Explore
+                    </a>
+                    <a href="/about" className="hover:text-gray-600">
+                        About
+                    </a>
+                    <a href="/sources" className="hover:text-gray-600">
+                        Sources
+                    </a>
+                </div>
             </div>
         </div>
     );
